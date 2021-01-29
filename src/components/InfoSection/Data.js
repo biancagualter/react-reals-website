eeafab6216adf91107aa130d6eca33f76a355b04
Bianca Gualter <bncgualter@gmail.com>
2021-01-29 14:26:27 -0300

    add data to info section

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'sobre',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Reals Premium',
+    headline: 'Transações ilimitadas com taxa zero',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dapibus pretium ornare. Donec sodales libero et nisl facilisis, vel consequat metus pellentesque.',
+    buttonLabel: 'Começar',
+    imgStart: false,
+    img: require('../../images/svg-1.svg'),
+    alt: 'Carro',
+    dark: true,
+    primary: true,
+    darkText: false
+}
--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const InfoContainer = styled.div `
     color: #fff;
-    background: ${({lightBg}) => (lightBg ? '#f9f9f9' : '#010606')};
+    background: ${({ lightBg }) => (lightBg ? '#f9f9f9' : '#010606')};
 
     @media screen and (max-width: 768px) {
         padding: 100px 0;
@@ -40,7 +40,7 @@ export const Column1 = styled.div `
 
 export const Column2 = styled.div `
     margin-bottom: 15px;
-    padding: 0 15px;
+    padding: 0 15px; 
     grid-area: col2;
 `
 
