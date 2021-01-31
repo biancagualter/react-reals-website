7deb65ec0e86b25252b841696e8dba46405b65fd
Bianca Gualter <bncgualter@gmail.com>
2021-01-31 06:25:36 -0300

    stylized footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa'
+import { FooterContainer, FooterWrap, FooterLinksContainer, FooterLinksWrapper, FooterLinksItems, FooterLinkTitle, FooterLink, SocialMediaWrap, SocialLogo, WebsiteRights, SocialIcons, SocialIconLink, SocialMedia } from './FooterElements'
 
 const Footer = () => {
     return (
@@ -15,37 +17,29 @@ const Footer = () => {
                             <FooterLink to="/signin">Investidores</FooterLink> 
                             <FooterLink to="/signin">Termos de Serviço</FooterLink> 
                         </FooterLinksItems>
-                        <FooterLinksItems>
-                            <FooterLinkTitle>Sobre nós</FooterLinkTitle>
-                            <FooterLink to="/signin">Quem somos</FooterLink> 
-                            <FooterLink to="/signin">Como funciona</FooterLink> 
-                            <FooterLink to="/signin">Avaliações</FooterLink> 
-                            <FooterLink to="/signin">Carreiras</FooterLink> 
-                            <FooterLink to="/signin">Investidores</FooterLink> 
-                            <FooterLink to="/signin">Termos de Serviço</FooterLink> 
-                        </FooterLinksItems>
                     </FooterLinksWrapper>
                     <FooterLinksWrapper>
                         <FooterLinksItems>
-                            <FooterLinkTitle>Sobre nós</FooterLinkTitle>
-                            <FooterLink to="/signin">Quem somos</FooterLink> 
-                            <FooterLink to="/signin">Como funciona</FooterLink> 
-                            <FooterLink to="/signin">Avaliações</FooterLink> 
-                            <FooterLink to="/signin">Carreiras</FooterLink> 
-                            <FooterLink to="/signin">Investidores</FooterLink> 
-                            <FooterLink to="/signin">Termos de Serviço</FooterLink> 
-                        </FooterLinksItems>
-                        <FooterLinksItems>
-                            <FooterLinkTitle>Sobre nós</FooterLinkTitle>
-                            <FooterLink to="/signin">Quem somos</FooterLink> 
-                            <FooterLink to="/signin">Como funciona</FooterLink> 
-                            <FooterLink to="/signin">Avaliações</FooterLink> 
-                            <FooterLink to="/signin">Carreiras</FooterLink> 
-                            <FooterLink to="/signin">Investidores</FooterLink> 
-                            <FooterLink to="/signin">Termos de Serviço</FooterLink> 
+                            <FooterLinkTitle>Fale Conosco</FooterLinkTitle>
+                            <FooterLink to="/signin">Chat Online</FooterLink> 
+                            <FooterLink to="/signin">E-mail</FooterLink> 
+                            <FooterLink to="/signin">Telefones</FooterLink> 
                         </FooterLinksItems>
                     </FooterLinksWrapper>
                 </FooterLinksContainer>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to="/">reals</SocialLogo>
+                        <WebsiteRights>reals © {new Date().getFullYear()} Todos os direitos reservados.</WebsiteRights>
+                        <SocialIcons>
+                            <SocialIconLink href="//www.facebook.com" target="_blank" aria-label="Facebook"><FaFacebook /></SocialIconLink>
+                            <SocialIconLink href="//www.instagram.com" target="_blank" aria-label="Instagram"><FaInstagram /></SocialIconLink>
+                            <SocialIconLink href="//www.youtube.com" target="_blank" aria-label="Youtube"><FaYoutube /></SocialIconLink>
+                            <SocialIconLink href="//www.twitter.com" target="_blank" aria-label="Twitter"><FaTwitter /></SocialIconLink>
+                            <SocialIconLink href="//www.linkedin.com" target="_blank" aria-label="Linkedin"><FaLinkedin /></SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
             </FooterWrap>
         </FooterContainer>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
-import HeroSection from '../components/HeroSection';
-import InfoSection from '../components/InfoSection';
-import { homeObjOne, homeObjTwo, homeObjThree } from '../components/InfoSection/Data';
-import Services from '../components/Services';
+import HeroSection from '../components/HeroSection'
+import InfoSection from '../components/InfoSection'
+import { homeObjOne, homeObjTwo, homeObjThree } from '../components/InfoSection/Data'
+import Services from '../components/Services'
+import Footer from '../components/Footer/index'
 
 const Home = () => {
     const [isOpen, setIsOpen] = useState(false)
